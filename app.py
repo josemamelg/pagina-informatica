06deaec33e4098bc109733b9c9e5e72ff54337f4
josemamelg <josemamelg@gmail.com>
2023-09-27 07:20:35 -0400

    mi primer commit

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-if __name__ == '__nain__':
-    app.run()
+@app.route('/')
+def index():
+    # Puedes agregar tu lógica de Python aquí para generar contenido dinámico.
+    contenido_python = "¡Hola desde Python!"
+
+    return render_template('index.html', contenido_python=contenido_python)
+
+if __name__ == '__main__':
+       app.run(debug=True)
